12e117c46e10365dee28e39e566601ecbcf3c5fd
mahshid aimaq <mahshid.aimaq@sjsu.edu>
2017-05-02 11:41:05 -0700

    All pages are in place but still need work

--- developmentUnited/js/scrolling-nav.js
+++ developmentUnited/js/scrolling-nav.js
@@ -17,33 +17,3 @@ $(function() {
         event.preventDefault();
     });
 });
-
-$(document).scroll(function () {
-
-    // parallaxing
-    var $movebg = $(window).scrollTop() * -0.3;
-    $('.portion').css('background-positionY', ($movebg) + 'px');
-
-    // add class active to nav a on scroll
-    var scrollPos = $(document).scrollTop() + 100;
-    $('nav a').each(function () {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-            $('nav a').removeClass("active");
-            currLink.addClass("active");
-        }
-    });
-
-    // changing padding of nav a on scroll
-    if (scrollPos > 250) {
-        $('nav a').addClass('small');
-        $('nav img').addClass('move');
-        $('nav span').removeClass('movetext');
-    } else {
-        $('nav a').removeClass('small');
-        $('nav img').removeClass('move');
-        $('nav span').addClass('movetext');
-    }
-
-});
